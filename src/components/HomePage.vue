76840bb8fb36ad4b47a477c298a90aef00fb6ba9
jiangyuesong <jiangyuesong@dxy.cn>
2022-08-26 14:47:27 +0800

    add try pages

---
+++ src/components/HomePage.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="h">
+    <el-container>
+      <el-aside left width="300px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <div style="width: 100%">
+            <NoteList />
+          </div>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import NoteList from "./NoteList.vue";
+export default {
+  name: "HomePage",
+  components: {
+    NoteList,
+  },
+};
+</script>
+
+<style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
---
+++ src/components/NoteList.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="box">
+    <div class="box" v-infinite-scroll="load" infinite-scroll-disabled="disabled" >
+        <ul class="list" >
+          <li v-for="i in count" class="list-item" :key="i">{{ i }}</li>
+        </ul>
+        <p v-if="loading" style="margin-top:10px;" class="loading">
+          <span></span>
+        </p>
+        <p v-if="noMore" style="margin-top:10px;font-size:13px;color:#ccc">没有更多了</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        count: 10,
+        loading: false
+      }
+    },
+    computed: {
+      noMore () {
+        return this.count >= 10000
+      },
+      disabled () {
+        return this.loading || this.noMore
+      }
+    },
+    methods: {
+      load () {
+        this.loading = true
+        setTimeout(() => {
+          this.count += 2
+          this.loading = false
+        }, 2000)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.el-container[data-v-2cb981da] {
+    width: -webkit-min-content;
+    width: -moz-min-content;
+    width: min-content;
+    max-width: 100%;
+    height: 100vh;
+    margin: 0 auto;
+}
+
+.el-container {
+    display: flex;
+    flex-direction: row;
+    flex: 1;
+    flex-basis: auto;
+    box-sizing: border-box;
+    min-width: 0;
+}
+#box{
+    width: 100%;
+  height: 100%;
+  position: absolute;
+  overflow-y: auto;
+}
+.box {
+  width: 100%;
+   margin:  0 auto;
+}
+.list {
+  padding: 0;
+  font-size: 14px;
+}
+.list-item {
+  width: 100%;
+  display: block;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  list-style: none;
+  padding: 0 1rem;
+  box-sizing: border-box;
+  height: 70px;
+  line-height: 70px;
+  border-bottom: 1px solid #e7e7e7;
+}
+.loading span {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border: 2px solid #409eff;
+  border-left: transparent;
+  animation: zhuan 0.5s linear infinite;
+  border-radius: 50%;
+}
+@keyframes zhuan {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
